ffe701ae1844ef4396b838df2adf651023c6fa3b
Casey Stratton <hullaballoonatic@gmail.com>
2026-04-18 15:58:46 -0500

    update to latest lazy. remove colorizer because it's not fixed.

--- home/.config/nvim/after/plugin/defaults.lua
+++ home/.config/nvim/after/plugin/defaults.lua
@@ -2,3 +2,8 @@
 vim.o.relativenumber = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+
+-- Disable warnings
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_privoder = 0
--- home/.config/nvim/lua/custom/plugins/colorizer.lua
+++
@@ -1,9 +0,0 @@
--- puts the color of a color code as the background of the color code text
--- https://github.com/norcalli/nvim-colorizer.lua
-
----@module 'lazy'
----@type LazySpec
-return {
-  'norcalli/nvim-colorizer.lua',
-  config = function() require('colorizer').setup() end,
-}
