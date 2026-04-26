678f89214cd5f98d92394106a0c70cc3ca4f9e00
CaseyStratton <casey.stratton@jbhunt.com>
2026-04-26 13:48:06 -0500

    i believe i missed things...

---
+++ home/dot-config/nvim/lua/custom/plugins/transparent.lua
@@ -0,0 +1,8 @@
+--Remove all background colors to make nvim transparent
+--https://github.com/xiyaowong/transparent.nvim
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  'xiyaowong/transparent.nvim',
+}
