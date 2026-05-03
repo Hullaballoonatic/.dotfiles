961094c01de91479704621a314daa662e5a198ff
Casey Stratton <hullaballoonatic@gmail.com>
2026-05-03 13:33:13 -0500

    navigate nvim and tmux panes with control + arrows

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dot Files
+
+For sharing my terminal setup and more between machines
+
+## Usage
+
+`sh bootstrap.sh` installs all applications and then configures them.
--- home/dot-config/nvim/after/plugin/keybinds.lua
+++ home/dot-config/nvim/after/plugin/keybinds.lua
@@ -1,3 +1,10 @@
+
+-- Switch panes with arrow keys, too
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-Left>', ':<C-U>TmuxNavigateLeft<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-Down>',  ':<C-U>TmuxNavigateDown<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-Up>',    ':<C-U>TmuxNavigateUp<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-Right>', ':<C-U>TmuxNavigateRight<cr>', { silent = true })
+
 -- Move lines up/down
 vim.keymap.set('n', '<A-Down>', ':m .+1<CR>==', { silent = true })
 vim.keymap.set('n', '<A-Up>', ':m .-2<CR>==', { silent = true })
@@ -5,3 +12,4 @@ vim.keymap.set('v', '<A-Down>', ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set('v', '<A-Up>', ":m '<-2<CR>gv=gv", { silent = true })
 vim.keymap.set('i', '<A-Down>', '<Esc>:m .+1<CR>==gi', { silent = true })
 vim.keymap.set('i', '<A-Up>', '<Esc>:m .-2<CR>==gi', { silent = true })
+
